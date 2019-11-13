884cc84cba07fd3311b18045c5d39667a8ef3588
zmjack <jack@zmland.com>
2019-11-13 22:04:00 +0800

    v1.0.0.1

---
+++ Def.Test/NugetTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Def.Test
+{
+    public class NuGetTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal($@"{NuGet.PackagesPath}def.test\1.0.0\", NuGet.PackageFolder(Assembly.GetExecutingAssembly()));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal($@"{NuGet.PackagesPath}def\1.2.3.4\", NuGet.PackageFolder("def", new Version(1, 2, 3, 4)));
+            Assert.Equal($@"{NuGet.PackagesPath}def\1.2.3\", NuGet.PackageFolder("def", new Version(1, 2, 3)));
+            Assert.Equal($@"{NuGet.PackagesPath}def\1.2.0\", NuGet.PackageFolder("def", new Version(1, 2)));
+        }
+
+    }
+}
